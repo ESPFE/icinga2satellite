0eb69058deed51e54f0f58eae6a4a304e4c5f481
André Sünnemann <a.suennemann@edv-peuker.de>
2018-01-01 05:04:10 +0100

    fix start.sh

--- config/opt/start.sh
+++ config/opt/start.sh
@@ -8,22 +8,22 @@ PKI_DIR='/etc/icinga2/pki'
 if [ ! -f '$CHECKFILE' ]; then
 	echo "[start.sh] Configuring Icinga2 Satellite..."
    
-	icinga2 pki save-cert
-		\ --host ${MASTER_HOST}
-		\ --port ${MASTER_PORT}
-		\ --key ${PKI_DIR}/local.key
-		\ --cert ${PKI_DIR}/local.crt
-		\ --trustedcert ${PKI_DIR}/${MASTER_HOST}.crt
+	icinga2 pki save-cert \
+		--host ${MASTER_HOST} \
+		--port ${MASTER_PORT} \
+		--key ${PKI_DIR}/local.key \
+		--cert ${PKI_DIR}/local.crt \
+		--trustedcert ${PKI_DIR}/${MASTER_HOST}.crt
    
-	icinga2 node setup
-		\ --accept-commands
-   		\ --accept-config
-   		\ --master_host ${MASTER_HOST}
-   		\ --endpoint ${MASTER_HOST}
-   		\ --ticket ${PKI_TICKET}
-   		\ --cn ${NODE_NAME}
-   		\ --zone ${NODE_ZONE}
-   		\ --trustedcert ${PKI_DIR}/${MASTER_HOST}.crt
+	icinga2 node setup \
+		--accept-commands \
+   		--accept-config \
+   		--master_host ${MASTER_HOST} \
+   		--endpoint ${MASTER_HOST} \
+   		--ticket ${PKI_TICKET} \
+   		--cn ${NODE_NAME} \
+   		--zone ${NODE_ZONE} \
+   		--trustedcert ${PKI_DIR}/${MASTER_HOST}.crt
    
 
 	if [ $? -eq 0 ]; then
